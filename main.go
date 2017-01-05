6431ddedcafd885a0802b7156395fbbfd2d0072c
= <=>
2017-01-05 19:40:52 +0330

    add filestore

--- main.go
+++ main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"reflect"
+	"os"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
 	"github.com/rs/xid"
 )
 
-// TODO: Implement Redis based session storage
-
 func main() {
 	log.SetFlags(log.Llongfile | log.Ldate | log.Lmicroseconds)
 	log.Println("Hello")
 
-	xidMap := make(map[string]bool)
+	//xidMap := make(map[string]bool)
 
 	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
 	if err != nil {
@@ -25,9 +24,9 @@ func main() {
 	}
 	defer conn.Close()
 
-	h := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	h.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			http.ServeFile(w, r, "./static/auth.html")
 		} else if r.Method == "POST" {
@@ -37,8 +36,8 @@ func main() {
 			log.Println("map", r.MultipartForm.Value)
 			if r.MultipartForm.Value["user"][0] == "qwerty" && r.MultipartForm.Value["password"][0] == "12345" {
 				token := xid.New().String()
-				xidMap[token] = true
-				_, err := conn.Do("set", token, "")
+				//				xidMap[token] = true
+				_, err := conn.Do("set", token, "", "EX", 100)
 				log.Println("Redis set", token)
 				if err != nil {
 					log.Fatal(err)
@@ -59,28 +58,49 @@ func main() {
 		log.Println("User reached /auth by method", r.Method)
 	})
 
-	h.HandleFunc("/suxess", func(w http.ResponseWriter, r *http.Request) {
-		cookie, errCookie := r.Cookie("auth")
-		if err != nil {
-			log.Fatal(err)
+	mux.HandleFunc("/suxess", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			cookie, errCookie := r.Cookie("auth")
+			if err != nil {
+				log.Fatal(err)
+			}
+			exists, err := redis.Bool(conn.Do("exists", cookie.Value))
+			log.Println("Redis get", cookie.Value, exists)
+			if err != nil {
+				log.Fatal(err)
+			}
+			//if cookie, err := r.Cookie("auth"); err == nil && xidMap[cookie.Value] {
+			if errCookie == nil && exists {
+				http.ServeFile(w, r, "./static/suxess.html")
+			} else {
+				w.Header().Set("Content-Type", "text/html")
+				fmt.Fprintln(w, "<h1><pre>(_O_) Go fuck yourself (_*_)")
+			}
+		} else if r.Method == "POST" {
+			//			r.ParseMultipartForm(32 << 20)
+			file, handler, err := r.FormFile("uploadfile")
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			defer file.Close()
+			f, err := os.OpenFile("./upload/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				panic(err)
+				return
+			}
+			log.Println("Open file", "./upload/"+handler.Filename)
+			defer f.Close()
+			_, err = io.Copy(f, file)
+			if err != nil {
+				panic(err)
+				return
+			}
+			http.Redirect(w, r, "http://localhost:9999/suxess", http.StatusFound)
 		}
-		exists, err := conn.Do("exists", cookie.Value)
-		exists = redis.Bool()
-		log.Println("Redis get", cookie.Value, valRedis, reflect.TypeOf(valRedis))
-		if err != nil {
-			log.Fatal(err)
-		}
-		//if cookie, err := r.Cookie("auth"); err == nil && xidMap[cookie.Value] {
-		if errCookie == nil && valRedis != nil {
-			http.ServeFile(w, r, "./static/suxess.html")
-		} else {
-			w.Header().Set("Content-Type", "text/html")
-			fmt.Fprintln(w, "<h1><pre>(_O_) Go fuck yourself (_*_)")
-		}
-
 	})
 
-	h.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("User reached /logout by method", r.Method)
 		if cookie, err := r.Cookie("auth"); err == nil {
 			log.Println("Logout cookie setter")
@@ -91,9 +111,10 @@ func main() {
 				Domain:   "localhost",
 				HttpOnly: true,
 				Expires:  time.Now(),
+				MaxAge:   -1,
 			})
-			_, err = conn.Do("set", cookie.Value, 0)
-			log.Println("Redis set", cookie.Value, 0)
+			_, err = conn.Do("del", cookie.Value)
+			log.Println("Redis del", cookie.Value)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -102,10 +123,16 @@ func main() {
 		//http.ServeFile(w, r, "./static/auth.html")
 	})
 
-	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/fileserver/", http.StripPrefix("/fileserver", http.FileServer(http.Dir("./upload/"))))
+
+	//	h.HandleFunc("/fileserver", func(w http.ResponseWriter, r *http.Request) {
+	//http.FileServer(http.Dir("/upload")
+	//	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(404)
 		fmt.Fprintln(w, "You're lost, go home")
 	})
-	err = http.ListenAndServe(":9999", h)
-	log.Fatal(err)
+	err = http.ListenAndServe(":9999", mux)
+	log.Println("Error listen and serve", err)
 }
